b4f6a2736aa8b2edfb641a48604a1c5675daab31
Kamil Marszałek <01187250@pw.edu.pl>
2024-11-30 23:59:36 +0100

    Fixed save expense bug

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/UserController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/UserController.java
@@ -7,29 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping("/user")
-public class UserController {
-    UserService userService;
-
-    @GetMapping("/all")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    @GetMapping("/{id}")
-    public User getUserById(@RequestParam long id) {
-        return userService.getUser(id);
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userService.saveUser(user);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable long id) {
-        userService.deleteUser(id);
-    }
+public class UserController extends GenericController<User, Long>{
+    public UserController(UserService service) {super(service);}
 }
