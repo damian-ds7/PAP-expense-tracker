7736a297e97d80fa7772a5fd08686a6701f9218c
Kamil Marszałek <01187250@pw.edu.pl>
2024-11-30 15:36:09 +0100

    Changed ExpenseApiApplication

--- ExpenseAPI/src/main/java/com/example/expenseapi/ExpenseApiApplication.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/ExpenseApiApplication.java
@@ -1,13 +1,41 @@
 package com.example.expenseapi;
 
+import com.example.expenseapi.pojo.Expense;
+import com.example.expenseapi.pojo.User;
+import com.example.expenseapi.repository.ExpenseRepository;
+import com.example.expenseapi.repository.UserRepository;
+import com.example.expenseapi.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ExpenseApiApplication {
+import java.util.Arrays;
 
+@SpringBootApplication
+public class ExpenseApiApplication implements CommandLineRunner {
+    @Autowired
+    ExpenseRepository expenseRepository;
+    @Autowired
+    UserRepository userRepository;
     public static void main(String[] args) {
         SpringApplication.run(ExpenseApiApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User[] users = new User[]{
+            new User("Herkules1", "Herkules1", "herkules1@gmail.com"),
+            new User("Herkules2", "Herkules2", "herkules2@gmail.com"),
+            new User("Herkules3", "Herkules3", "herkules3@gmail.com")
+        };
+        userRepository.saveAll(Arrays.asList(users));
+        Expense[] expenses = new Expense[]{
+                new Expense(100),
+                new Expense(200),
+                new Expense(300),
+        };
+        expenseRepository.saveAll(Arrays.asList(expenses));
+
+    }
 }
