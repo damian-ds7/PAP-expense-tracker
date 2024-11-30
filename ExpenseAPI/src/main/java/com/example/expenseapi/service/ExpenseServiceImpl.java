3181246f829f88361c7fc11690e1042291e8d3dc
Kamil Marszałek <01187250@pw.edu.pl>
2024-11-30 15:34:45 +0100

    Added ExpenseService implementation

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/ExpenseServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.expenseapi.service;
+
+import com.example.expenseapi.pojo.Expense;
+import com.example.expenseapi.repository.ExpenseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ExpenseServiceImpl implements ExpenseService{
+
+    ExpenseRepository expenseRepository;
+
+    @Override
+    public Expense getExpense(Long id) {
+        Optional<Expense> expense = expenseRepository.findById(id);
+        return unwrapExpense(expense, id);
+    }
+
+    @Override
+    public List<Expense> getAllExpenses() {
+        return (List<Expense>) expenseRepository.findAll();
+    }
+
+    @Override
+    public Expense saveExpense(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    @Override
+    public void deleteExpense(Long id) {
+        expenseRepository.deleteById(id);
+    }
+    static Expense unwrapExpense(Optional<Expense> expense, Long id) {
+        return expense.orElse(null);
+    }
+}
