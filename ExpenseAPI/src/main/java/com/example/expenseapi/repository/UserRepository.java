24e2af57c19108b11224a973fbb0851c37bd8da3
Kamil Marszałek <01187250@pw.edu.pl>
2024-11-30 15:35:12 +0100

    Added UserRepository

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.expenseapi.repository;
+
+import com.example.expenseapi.pojo.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
