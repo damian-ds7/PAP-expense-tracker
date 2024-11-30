386c1e4d8cd29e1c96d559e96229884aec8afa3c
ddsouza <damian.dsouza.stud@pw.edu.pl>
2024-11-30 21:59:49 +0100

    Refactored TopSection, added records to HomeScreen

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/HomeScreen.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/HomeScreen.kt
@@ -1,64 +1,39 @@
 package pw.edu.pl.pap.ui.home
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import pw.edu.pl.pap.data.Home
 import pw.edu.pl.pap.viewmodel.HomeViewModel
 import pw.edu.pl.pap.ui.common.LoadingScreen
-
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
     var isLoading by remember { mutableStateOf(true) }
-    val homeInfo = viewModel.homeInfo.collectAsState().value
+    val homeInfo = viewModel.expensesInfo.collectAsState().value
+    val records = viewModel.records.collectAsState().value
 
     LaunchedEffect(Unit) {
         viewModel.fetchHomeInfo()
+        viewModel.fetchRecords()
         isLoading = false
     }
 
     if (isLoading) {
         LoadingScreen()
     } else if (homeInfo != null) {
-        TopSection(homeInfo)
-    } else {
-        return
-    }
-}
+        Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+            TopSection(homeInfo)
 
-@Composable
-fun TopSection(homeInfo: Home) {
-    Row(Modifier.fillMaxWidth().padding(vertical = 8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-        Column(Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = "My Expenses",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium
-            )
-            Text(
-                text = "${homeInfo.userExpenses} zł",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium
-            )
-        }
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Column(Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(
-                text = "Total Expenses",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium
-            )
-            Text(
-                text = "${homeInfo.expenses} zł",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Medium
-            )
+            LazyColumn {
+                items(records) { record ->
+                    RecordBlock(record)
+                }
+            }
         }
     }
 }
--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/TopSection.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/home/TopSection.kt
@@ -1,2 +1,42 @@
 package pw.edu.pl.pap.ui.home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pw.edu.pl.pap.data.InitialExpenses
+
+@Composable
+fun TopSection(homeInfo: InitialExpenses) {
+    Row(Modifier.fillMaxWidth().padding(vertical = 8.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+        Column(Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "My Expenses",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            )
+            Text(
+                text = "${homeInfo.userExpenses} zł",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
+
+        Column(Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = "Total Expenses",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            )
+            Text(
+                text = "${homeInfo.expenses} zł",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
+    }
+}
