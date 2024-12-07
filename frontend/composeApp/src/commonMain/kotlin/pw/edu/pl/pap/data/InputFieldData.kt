65cea92a783b85c918f07276f6e5628dfc1cd337
ddsouza <01187217@pw.edu.pl>
2024-12-07 21:57:25 +0100

    Removed unnecessary param

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/data/InputFieldData.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/data/InputFieldData.kt
@@ -8,6 +8,5 @@ data class InputFieldData(
     val title: String,
     var parameter: MutableState<String>,
     val onChange: (String) -> Unit,
-    val defaultValue: String = "",
     val keyboardOptions: KeyboardOptions? = null,  // Optional keyboard options, used for number only keyboard
 )
