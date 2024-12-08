3e41c161e8c9f2f73404f8735165f43576071c26
msuski <01187288@pw.edu.pl>
2024-12-08 12:23:53 +0100

    Added code for dropdown list

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/common/InputFields.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/common/InputFields.kt
@@ -1,19 +1,19 @@
 package pw.edu.pl.pap.ui.common
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -22,6 +22,9 @@ import pw.edu.pl.pap.data.inputFields.InputFieldData
 import pw.edu.pl.pap.data.inputFields.TextFieldData
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 
 
 @Composable
@@ -80,12 +83,63 @@ fun createDropdownList(
     data: DropdownListData
 ){
     var showDropdown by remember { mutableStateOf(false) }
-    Box(
-        modifier = Modifier
-            .clickable { showDropdown = !showDropdown },
-        contentAlignment = Alignment.Center
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+
+        // button
+        Box(
+            modifier = Modifier
+                .clickable { showDropdown = true },
+            contentAlignment = Alignment.Center
         )
-    {
-        Text(text = data.itemList[data.selectedIndex.value], modifier = Modifier.padding(3.dp))
+        {
+            Text(text = data.itemList[data.selectedIndex.value], modifier = Modifier.padding(3.dp))
+        }
+
+        // dropdown list
+        Box() {
+            if (showDropdown) {
+                Popup(
+                    alignment = Alignment.TopCenter,
+//                properties = PopupProperties(
+//                    excludeFromSystemGesture = true,
+//                ),
+                    // to dismiss on click outside
+                    onDismissRequest = { showDropdown = false }
+                ) {
+
+                    Column(
+                        modifier = Modifier
+                            .heightIn(max = 90.dp)
+                            .verticalScroll(state = scrollState)
+                            .border(width = 1.dp, color = Color.Gray),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+
+                        data.itemList.onEachIndexed { index, item ->
+                            if (index != 0) {
+                                Divider(thickness = 1.dp, color = Color.LightGray)
+                            }
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        data.onItemClick(index)
+                                        showDropdown = !showDropdown
+                                    },
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(text = item)
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
     }
 }
