20f88e06ac436d8a017e260d8fef56a887771de9
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-18 21:27:55 +0100

    Refactoring after changing user group to group

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/UserGroupServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/UserGroupServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.expenseapi.service;
 
-import com.example.expenseapi.pojo.UserGroup;
+import com.example.expenseapi.pojo.Group;
 import com.example.expenseapi.repository.UserGroupRepository;
 
-public class UserGroupServiceImpl extends GenericServiceImpl<UserGroup, Long> implements UserGroupService{
+public class UserGroupServiceImpl extends GenericServiceImpl<Group, Long> implements UserGroupService{
     public UserGroupServiceImpl(UserGroupRepository repository) {
         super(repository);
     }
