cc45e11de525b2f04b6f9308e417fb6dfbac6da4
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-18 21:47:55 +0100

    Implemented Currency

--- ExpenseAPI/src/main/java/com/example/expenseapi/pojo/Currency.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/Currency.java
@@ -1,4 +1,29 @@
 package com.example.expenseapi.pojo;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "currencies")
 public class Currency {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NonNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NonNull
+    @Column(name = "symbol", nullable = false)
+    private String symbol;
+
+    @NonNull
+    @Column(name = "exchange_rate", nullable = false)
+    private double exchangeRate;
 }
