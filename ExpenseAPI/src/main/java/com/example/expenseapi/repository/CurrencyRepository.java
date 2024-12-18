e0049d641767baa00fa5a22779c081e5deb268ea
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-18 22:47:28 +0100

    Fixed bug

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/CurrencyRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/CurrencyRepository.java
@@ -3,4 +3,4 @@ package com.example.expenseapi.repository;
 import com.example.expenseapi.pojo.Currency;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CurrencyRepository extends CrudRepository<Currency, Integer> {}
+public interface CurrencyRepository extends CrudRepository<Currency, Long> {}
