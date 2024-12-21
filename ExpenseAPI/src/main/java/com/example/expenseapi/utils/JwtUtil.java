f3fec05baaa690d2a8b63ba3486e3572f12c2ba4
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-21 11:32:51 +0100

    Improved code for parsing

--- ExpenseAPI/src/main/java/com/example/expenseapi/utils/JwtUtil.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/utils/JwtUtil.java
@@ -16,12 +16,17 @@ public class JwtUtil {
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SECRET_KEY, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String extractEmail(String token) {
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public boolean validateToken(String token, String email) {
@@ -29,6 +34,12 @@ public class JwtUtil {
     }
 
     private boolean isTokenExpired(String token){
-        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody().getExpiration().before(new Date());
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
     }
 }
