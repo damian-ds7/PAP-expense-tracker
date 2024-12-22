111d1eee676026959b47efb8d4bbbd3df9b065d3
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-22 23:14:55 +0100

    Deleted unused imports

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/RoleRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/RoleRepository.java
@@ -2,7 +2,6 @@ package com.example.expenseapi.repository;
 
 import com.example.expenseapi.pojo.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
