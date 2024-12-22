f2d19335db456560473812cfe2c34932a2dbbe29
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-22 12:32:57 +0100

    Fixed bug - no errors now

--- ExpenseAPI/src/main/java/com/example/expenseapi/pojo/BaseGroup.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/BaseGroup.java
@@ -15,7 +15,12 @@ import lombok.Setter;
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class BaseGroup {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "base_group_gen")
+    @SequenceGenerator(
+            name = "base_group_gen",
+            sequenceName = "base_group_seq",
+            allocationSize = 1
+    )
     protected Long id;
 
     @Column(name = "name")
--- ExpenseAPI/src/main/java/com/example/expenseapi/pojo/Category.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/Category.java
@@ -13,7 +13,7 @@ import lombok.*;
 
 public class Category {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
