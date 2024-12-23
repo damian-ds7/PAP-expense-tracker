a97e120a2216db09b3d053289e1d19f7c1f9a2bb
ddsouza <damian.dsouza.stud@pw.edu.pl>
2024-12-23 12:09:50 +0100

    Fixed imports

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/loginSystem/ButtonRow.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/loginSystem/ButtonRow.kt
@@ -2,18 +2,13 @@ package pw.edu.pl.pap.ui.loginSystem
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import pw.edu.pl.pap.navigation.HomeScreenComponent
-import pw.edu.pl.pap.navigation.loginSystem.BaseLoginScreenComponent
+import pw.edu.pl.pap.screenComponents.loginSystem.BaseLoginScreenComponent
 import pw.edu.pl.pap.ui.common.TextButton
-import pw.edu.pl.pap.ui.home.sortingSystem.GroupButton
-import pw.edu.pl.pap.ui.home.sortingSystem.RefreshButton
 
 @Composable
 fun LogInSignUpButtonRow(component: BaseLoginScreenComponent, text: String, scope: CoroutineScope) {
