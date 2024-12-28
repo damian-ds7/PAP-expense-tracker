eae48bba8f22888d4b0c298c31dfe5301920579f
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-28 20:33:04 +0100

    Created TemporaryMembershipService

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/TemporaryMembershipService.java
@@ -0,0 +1,6 @@
+package com.example.expenseapi.service;
+
+import com.example.expenseapi.pojo.TemporaryMembership;
+
+public interface TemporaryMembershipService extends GenericService<TemporaryMembership, Long> {
+}
