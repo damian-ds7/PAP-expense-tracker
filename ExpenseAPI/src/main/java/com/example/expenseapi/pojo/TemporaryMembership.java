1432e6b67197609aeb2a48265057667c659b9087
Kamil Marszałek <01187250@pw.edu.pl>
2024-12-28 20:22:10 +0100

    Added TemporaryMembership entity

---
+++ ExpenseAPI/src/main/java/com/example/expenseapi/pojo/TemporaryMembership.java
@@ -0,0 +1,18 @@
+package com.example.expenseapi.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "temporary_memberships")
+public class TemporaryMembership extends BaseMembership {
+    public TemporaryMembership(User user, BaseGroup group, String name, Role role) {
+        super(user, group, name, role);
+    }
+}
