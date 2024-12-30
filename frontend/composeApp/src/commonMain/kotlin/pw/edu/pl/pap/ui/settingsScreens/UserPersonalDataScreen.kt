755169426fbbbfaeb28f885baefadf12afe3f673
msuski <01187288@pw.edu.pl>
2024-12-30 13:52:13 +0100

    Refactored ConfirmOrBackButtonRow

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/settingsScreens/UserPersonalDataScreen.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/ui/settingsScreens/UserPersonalDataScreen.kt
@@ -2,8 +2,16 @@ package pw.edu.pl.pap.ui.settingsScreens
 
 import androidx.compose.runtime.Composable
 import pw.edu.pl.pap.screenComponents.settingsScreens.UserPersonalDataScreenComponent
+import pw.edu.pl.pap.ui.common.Header
+import pw.edu.pl.pap.ui.common.InputFields
 
 @Composable
 fun UserPersonalDataScreen(component: UserPersonalDataScreenComponent) {
+
+    Header("Personal data")
+
+    component.setupInputFields()
+    InputFields(component.inputFieldsData)
+
 
 }
