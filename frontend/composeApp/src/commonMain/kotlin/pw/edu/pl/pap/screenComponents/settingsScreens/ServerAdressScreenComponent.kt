1b5438861fe1819b6f915b84f2bc7bbef1c934be
msuski <01187288@pw.edu.pl>
2025-01-01 14:44:59 +0100

    Implemented ServerAdressScreenComponent

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
@@ -2,6 +2,10 @@ package pw.edu.pl.pap.screenComponents.settingsScreens
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.launch
+import pw.edu.pl.pap.data.uiSetup.ConfirmationDialogConfig
+import pw.edu.pl.pap.data.uiSetup.inputFields.InputFieldData
+import pw.edu.pl.pap.data.uiSetup.inputFields.TextFieldData
 
 
 class ServerAdressScreenComponent (
@@ -11,8 +15,35 @@ class ServerAdressScreenComponent (
 
     private var serverAddress: MutableState<String> = mutableStateOf(apiService.getCurrentUrl())
 
+    override var confirmationData = ConfirmationDialogConfig(
+        mainText = "Change Server Address",
+        subText = "Are you sure you want to change server address?",
+        onNo = { showConfirmationDialog.value = false },
+        onYes = {
+            showConfirmationDialog.value = false
+            coroutineScope.launch { postChanges() }
+            onBack()
+        }
+    )
 
-    private fun updateUrl() {
+    override fun postChanges() {
         apiService.updateBaseUrl(serverAddress.value)
+    }
+
+    override fun setupInputFields() {
+        _inputFieldsData.clear()
+        _inputFieldsData.addAll(
+            listOf(
+                InputFieldData(
+                    title = "Server address: ",
+                    textFieldData = TextFieldData(
+                        parameter = serverAddress,
+                        onChange = {
+                            coroutineScope.launch { serverAddress.value = it }
+                        }
+                    )
+                )
+            )
+        )
     }
 }
