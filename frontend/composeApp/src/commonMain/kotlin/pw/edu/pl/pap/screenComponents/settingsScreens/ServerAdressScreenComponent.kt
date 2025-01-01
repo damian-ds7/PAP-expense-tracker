54f4e5cbbdfcf692b61e8cc9b4bfec0209e4f415
msuski <01187288@pw.edu.pl>
2025-01-01 14:41:08 +0100

    Adjusted main SettingsScreenComponent

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/settingsScreens/ServerAdressScreenComponent.kt
@@ -1,8 +1,18 @@
 package pw.edu.pl.pap.screenComponents.settingsScreens
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
 
 class ServerAdressScreenComponent (
     baseSettingsScreenComponent: BaseSettingsScreenComponent
 ) : BaseSettingsScreenComponentImpl(baseSettingsScreenComponent) {
 
+
+    private var serverAddress: MutableState<String> = mutableStateOf(apiService.getCurrentUrl())
+
+
+    private fun updateUrl() {
+        apiService.updateBaseUrl(serverAddress.value)
+    }
 }
