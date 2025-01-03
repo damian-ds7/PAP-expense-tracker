60bab7768d3e01babb1b2d0c580f979fbe4306bd
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-03 09:49:20 +0100

    Added documentation to FilterController

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/FilterController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/FilterController.java
@@ -2,7 +2,13 @@ package com.example.expenseapi.web;
 
 import com.example.expenseapi.dto.ExpenseDTO;
 import com.example.expenseapi.dto.ExpenseFilter;
+import com.example.expenseapi.pojo.Expense;
 import com.example.expenseapi.service.ExpenseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +27,8 @@ public class FilterController {
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Retrieves expenses which satisfies conditions")
+    @ApiResponse(responseCode = "200", description = "List of expense objects satisfying conditions", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ExpenseDTO.class))))
     public ResponseEntity<List<ExpenseDTO>> search(
             @RequestParam(required = false) List<String> categories,
             @RequestParam(required = false) String groupName,
