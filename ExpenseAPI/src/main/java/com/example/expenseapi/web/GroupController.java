a566ed640e0f4f8fc7cb64e2e71b04b04f71bf02
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-03 09:55:07 +0100

    Added docs to GroupController

--- ExpenseAPI/src/main/java/com/example/expenseapi/web/GroupController.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/web/GroupController.java
@@ -4,6 +4,12 @@ import com.example.expenseapi.dto.UserDTO;
 import com.example.expenseapi.pojo.Group;
 import com.example.expenseapi.service.MembershipService;
 import com.example.expenseapi.service.GroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/group")
+@Tag(name="Group Controller", description = "Controller to manage group objects")
 public class GroupController extends GenericController<Group, Long>{
     private final MembershipService membershipService;
 
@@ -24,11 +31,19 @@ public class GroupController extends GenericController<Group, Long>{
     }
 
     @GetMapping("/members/{group}")
+    @Operation(summary = "Get members of a group")
+    @ApiResponse(responseCode = "200", description = "List of group members.",
+            content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = UserDTO.class))))
     public ResponseEntity<List<UserDTO>> getMembers(@PathVariable String group){
         return new ResponseEntity<>(membershipService.findUsers(group), HttpStatus.OK);
     }
 
     @GetMapping("/admins/{group}")
+    @Operation(summary = "Get administrators of a group")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved list of group administrators.",
+            content = @Content(mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = UserDTO.class))))
     public ResponseEntity<List<UserDTO>> getAdmins(@PathVariable String group){
         return new ResponseEntity<>(membershipService.findAdmins(group), HttpStatus.OK);
     }
