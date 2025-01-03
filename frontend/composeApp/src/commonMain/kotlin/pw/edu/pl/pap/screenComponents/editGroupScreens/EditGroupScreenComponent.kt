761d1d9ab4b2cd080a5b8435ae1e69f19c2e3822
msuski <01187288@pw.edu.pl>
2025-01-03 18:12:17 +0100

    Implemented EditGroupScreenComponent

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/editGroupScreens/EditGroupScreenComponent.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/screenComponents/editGroupScreens/EditGroupScreenComponent.kt
@@ -1,5 +1,9 @@
 package pw.edu.pl.pap.screenComponents.editGroupScreens
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import io.ktor.http.*
+import kotlinx.coroutines.launch
 import pw.edu.pl.pap.data.databaseAssociatedData.Expense
 import pw.edu.pl.pap.data.databaseAssociatedData.UserGroup
 import pw.edu.pl.pap.screenComponents.mainScreens.BaseScreenComponent
@@ -13,4 +17,33 @@ class EditGroupScreenComponent(
     private val group: UserGroup
 ) : BaseGroupEditScreenComponent(baseComponent, onDismiss, onSave) {
 
+    override var name: MutableState<String> = mutableStateOf(group.name)
+
+    override fun confirm() {
+        val newGroup = group.copy(name = name.value)
+
+        if (newGroup == group) {
+            onDismiss()
+            return
+        }
+
+        coroutineScope.launch {
+//            if (apiService.expenseApiClient.updateGroup(newGroup).status.isSuccess()) {
+//                onSave()
+//            }
+            //TODO
+        }
+
+        println("Updated Expense ${newGroup.id} from ${group.name} to ${newGroup.name}")
+    }
+
+    fun deleteExpense() {
+        println("Deleting expense $group")
+        coroutineScope.launch {
+//            if (apiService.expenseApiClient.deleteGroup(group.id).status.isSuccess()) {
+//                onDelete()
+//            }
+            //TODO
+        }
+    }
 }
