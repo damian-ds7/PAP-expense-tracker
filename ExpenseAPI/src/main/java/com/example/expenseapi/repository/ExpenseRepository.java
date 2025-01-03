97c7fa1bb53e8faa330c46468b65429211e340ea
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-01-03 22:10:56 +0100

    removed unused import

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/ExpenseRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/ExpenseRepository.java
@@ -4,7 +4,6 @@ import com.example.expenseapi.pojo.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
-import java.util.Optional;
 
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long>, JpaSpecificationExecutor<Expense> {
