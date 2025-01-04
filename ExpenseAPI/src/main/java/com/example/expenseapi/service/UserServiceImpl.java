509fa3b660aa0d06de35510a97f1e69820bacf1e
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-04 22:01:33 +0100

    Implemented service method for updating password

--- ExpenseAPI/src/main/java/com/example/expenseapi/service/UserServiceImpl.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/service/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.example.expenseapi.service;
 
+import com.example.expenseapi.dto.ChangePasswordDTO;
 import com.example.expenseapi.dto.UserDTO;
 import com.example.expenseapi.filter.UserFilter;
 import com.example.expenseapi.mapper.UserMapper;
 import com.example.expenseapi.pojo.User;
 import com.example.expenseapi.repository.UserRepository;
 import com.example.expenseapi.specification.UserSpecification;
+import com.example.expenseapi.utils.AuthHelper;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,11 +20,13 @@ import java.util.Optional;
 public class UserServiceImpl extends GenericServiceImpl<User, Long> implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository repository, UserRepository userRepository, UserMapper userMapper) {
+    public UserServiceImpl(UserRepository repository, UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         super(repository);
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -39,6 +44,19 @@ public class UserServiceImpl extends GenericServiceImpl<User, Long> implements U
                 .map(userMapper::userToUserDTO)
                 .toList();
 
+    }
+
+    @Override
+    public UserDTO changePassword(ChangePasswordDTO passwordDTO) {
+        Optional<User> user = findByEmail(AuthHelper.getUserEmail());
+        UserDTO response = null;
+        if (user.isPresent()) {
+            User temp = user.get();
+            temp.setPassword(passwordEncoder.encode(passwordDTO.getNewPassword()));
+            userRepository.save(temp);
+            response = userMapper.userToUserDTO(temp);
+        }
+        return response;
     }
 }
 
