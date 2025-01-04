3c5222439e08777061ed15ec6bac381a92879045
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-01-04 03:04:24 +0100

    Basic preference controller implemented

--- ExpenseAPI/src/main/java/com/example/expenseapi/repository/PreferenceRepository.java
+++ ExpenseAPI/src/main/java/com/example/expenseapi/repository/PreferenceRepository.java
@@ -3,4 +3,6 @@ package com.example.expenseapi.repository;
 import com.example.expenseapi.pojo.Preference;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PreferenceRepository extends JpaRepository<Preference, Long> {}
+public interface PreferenceRepository extends JpaRepository<Preference, Long> {
+    Preference getPreferenceById(Long id);
+}
