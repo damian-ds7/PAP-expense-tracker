d1c55cf1b2930ccdf1d70560a7842e6760ec7fba
msuski <01187288@pw.edu.pl>
2025-01-08 13:11:21 +0100

    Implemented UserApiClient

---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/endpoints/UserEndpoints.kt
@@ -0,0 +1,5 @@
+package pw.edu.pl.pap.api.endpoints
+
+sealed class UserEndpoints(relativePath: String) : BaseEndpoint("/user", relativePath) {
+    data class Search(val group: String): UserEndpoints("/search/$group")
+}
