676edb46faf888519d422be09e8c4eb1113c472f
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-10 13:55:32 +0100

    removed old api clients

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/endpoints/CurrencyEndpoint.kt
+++
@@ -1,5 +0,0 @@
-package pw.edu.pl.pap.api.endpoints
-
-open class CurrencyEndpoint(relativePath: String) : BaseEndpoint("/currency", relativePath) {
-    data object AllCurrencies : CurrencyEndpoint("/all")
-}
--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/api/endpoints/PaymentMethodEndpoint.kt
+++
@@ -1,5 +0,0 @@
-package pw.edu.pl.pap.api.endpoints
-
-open class PaymentMethodEndpoint(relativePath: String) : BaseEndpoint("/method", relativePath) {
-    data object AllMethods : PaymentMethodEndpoint("/all")
-}
