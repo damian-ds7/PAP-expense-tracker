4174a490f10d6b90bef02c257ba898b61ce4cb16
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-01-10 01:51:35 +0100

    written simple test cases for refresh tokens operations

--- ExpenseAPI/src/test/java/com/example/expenseapi/controller/AuthControllerIT.java
+++ ExpenseAPI/src/test/java/com/example/expenseapi/controller/AuthControllerIT.java
@@ -1,5 +1,9 @@
 package com.example.expenseapi.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.example.expenseapi.repository.RefreshTokenRepository;
+import com.jayway.jsonpath.JsonPath;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,10 +19,12 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class AuthControllerIT {
     private final MockMvc mockMvc;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
-    public AuthControllerIT(MockMvc mockMvc) {
+    public AuthControllerIT(MockMvc mockMvc, RefreshTokenRepository refreshTokenRepository) {
         this.mockMvc = mockMvc;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     @Test
@@ -27,6 +33,7 @@ public class AuthControllerIT {
                 .andExpect(MockMvcResultMatchers.status().is4xxClientError());
     }
 
+    @Transactional
     @Test
     void testLoginSuccessful() throws Exception {
         String jsonRequestBody = """
@@ -81,8 +88,8 @@ public class AuthControllerIT {
                 }
                 """;
         mockMvc.perform(MockMvcRequestBuilders.post("/auth/register")
-                        .content(jsonRequestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
+                .content(jsonRequestBody)
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
     }
     @Test
@@ -96,13 +103,88 @@ public class AuthControllerIT {
                 }
                 """;
         mockMvc.perform(MockMvcRequestBuilders.post("/auth/register")
-                        .content(jsonRequestBody)
-                        .contentType(MediaType.APPLICATION_JSON))
+                .content(jsonRequestBody)
+                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isConflict());
     }
 
     @Test
-    void testRefresh() throws Exception {
-
+    void testRefreshUserWithActiveToken() throws Exception {
+        String jsonRequestBody = """
+                {
+                "refreshToken": "token1"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
+                .content(jsonRequestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").exists());
     }
+
+    @Test
+    void testRefreshUserWithExpiredToken() throws Exception {
+        String jsonRequestBody = """
+                {
+                "refreshToken": "token2"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
+                .content(jsonRequestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+        assertThat(refreshTokenRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    void testRefreshUserWithNonExistingToken() throws Exception {
+        String jsonRequestBody = """
+                {
+                "refreshToken": "token3"
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
+                .content(jsonRequestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void testRefreshUserWithEmptyToken() throws Exception {
+        String jsonRequestBody = """
+                {
+                "refreshToken": ""
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
+                .content(jsonRequestBody)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
+    void testLoginAndRefresh() throws Exception {
+        String jsonRequestBody = """
+                {
+                    "email": "herkules1@gmail.com",
+                    "password": "123"
+                }
+                """;
+        String result = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                        .content(jsonRequestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse().getContentAsString();
+        String refreshToken = JsonPath.read(result, "$.refreshToken");
+        String jsonRequestBodyRefresh = """
+                {
+                "refreshToken": "%s"
+                }
+                """.formatted(refreshToken);
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/refresh")
+                        .content(jsonRequestBodyRefresh)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").exists());
+    }
+
 }
