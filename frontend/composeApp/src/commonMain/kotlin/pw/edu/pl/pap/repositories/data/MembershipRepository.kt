8aec756332a4133701a7ebd4e6d1d10ece1d12bc
msuski <01187288@pw.edu.pl>
2025-01-11 11:44:40 +0100

    Created following repositories

---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/repositories/data/MembershipRepository.kt
@@ -0,0 +1,8 @@
+package pw.edu.pl.pap.repositories.data
+
+import pw.edu.pl.pap.api.data.MembershipApi
+
+class MembershipRepository(val api: MembershipApi) {
+
+
+}
---
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/repositories/data/TemporaryMembershipRepository.kt
@@ -0,0 +1,6 @@
+package pw.edu.pl.pap.repositories.data
+
+import pw.edu.pl.pap.api.data.TemporaryMembershipApi
+
+class TemporaryMembershipRepository(val api: TemporaryMembershipApi) {
+}
