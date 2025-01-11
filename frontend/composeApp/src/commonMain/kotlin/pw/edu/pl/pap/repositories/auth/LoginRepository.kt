dc1a93b8f719b33e218e6c582dbc463ee20a7b7d
ddsouza <damian.dsouza.stud@pw.edu.pl>
2025-01-11 01:38:15 +0100

    modified loginRepository to include tokenRepository

--- frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/repositories/auth/LoginRepository.kt
+++ frontend/composeApp/src/commonMain/kotlin/pw/edu/pl/pap/repositories/auth/LoginRepository.kt
@@ -3,9 +3,10 @@ package pw.edu.pl.pap.repositories.auth
 import io.ktor.client.call.*
 import io.ktor.http.*
 import pw.edu.pl.pap.api.auth.LoginApi
+import pw.edu.pl.pap.data.databaseAssociatedData.Tokens
 import pw.edu.pl.pap.data.databaseAssociatedData.UserLoginData
 
-class LoginRepository(private val loginApi: LoginApi, private val tokenProvider: TokenRepository) {
+class LoginRepository(private val loginApi: LoginApi, private val tokenRepository: TokenRepository) {
     suspend fun login(userLoginData: UserLoginData): Boolean {
         val response = loginApi.login(userLoginData)
 
@@ -14,7 +15,7 @@ class LoginRepository(private val loginApi: LoginApi, private val tokenProvider:
             return false
         }
 
-        tokenProvider.setToken(response.body<String>())
+        tokenRepository.setTokens(response.body<Tokens>())
         return true
     }
 }
