491290e86867d31d2c2bd3bbb80fd4504af25acd
Michał Szwejk <michal.szwejk.stud@pw.edu.pl>
2025-01-12 15:43:58 +0100

    renamed exception

--- ExpenseAPI/src/main/java/com/example/expenseapi/exception/RoleNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.example.expenseapi.exception;
-
-public class RoleNotFound extends RuntimeException {
-    public RoleNotFound(Long id) {
-        super("Role with id " + id + " not found");
-    }
-}
