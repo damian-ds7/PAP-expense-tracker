b75e0cb9da6f1a93df3d541afa5b1ff8dd498ae5
Kamil Marszałek <01187250@pw.edu.pl>
2025-01-12 23:43:57 +0100

    Small fix

--- ExpenseAPI/src/test/java/com/example/expenseapi/controller/CategoryControllerIT.java
+++ ExpenseAPI/src/test/java/com/example/expenseapi/controller/CategoryControllerIT.java
@@ -1,6 +1,5 @@
 package com.example.expenseapi.controller;
 
-import com.example.expenseapi.utils.TokenGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,13 +14,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class CategoryControllerIT {
     private final MockMvc mockMvc;
-    private final String activeUser = "herkules1@gmail.com";
-    private final TokenGenerator gen;
 
     @Autowired
-    public CategoryControllerIT(MockMvc mockMvc, TokenGenerator gen) {
+    public CategoryControllerIT(MockMvc mockMvc) {
         this.mockMvc = mockMvc;
-        this.gen = gen;
     }
 
     @Test
@@ -31,34 +27,5 @@ public class CategoryControllerIT {
                 .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("food"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("transport"));
-    }
-
-    @Test
-    void testCreateCategory() throws Exception {
-        String jsonBody = """
-                {
-                    "name": "bills"
-                }
-                """;
-        mockMvc.perform(MockMvcRequestBuilders.post("/category/insert")
-                .header("Authorization", "Bearer " + gen.getToken(activeUser))
-                .contentType("application/json")
-                        .content(jsonBody))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
-    }
-
-    @Test
-    void testUpdateCategory() throws Exception {
-        String jsonBody = """
-                {
-                    "id": 1,
-                    "name": "bills"
-                }
-                """;
-        mockMvc.perform(MockMvcRequestBuilders.put("/category/update/1")
-                        .header("Authorization", "Bearer " + gen.getToken(activeUser))
-                        .contentType("application/json")
-                        .content(jsonBody))
-                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
